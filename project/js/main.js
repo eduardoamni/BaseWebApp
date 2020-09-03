f65baa080cd9b38699d5efb053a2d1d515d76252
Eduardo Amigo Nieto <eduardo.amigo@servilia.local>
2020-09-03 11:34:49 +0200

    añadido el tiempo

--- project/js/main.js
+++ project/js/main.js
@@ -1,9 +1,14 @@
-function showPicture(){
-  // use jQuery ($ is shorthand) to find the div on the page and then change the html
-  // 'rounded-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
-  $("#image").append('<img class="rounded-circle" src="images/high-five.gif"/>');
-  $("p").html("High five! You're building your first web app!");
+$(document).ready(function(){
+  getWeather();
+})
 
-  // jQuery can do a lot of crazy stuff, so make sure to Google around to find out more
-  
+function getWeather(){
+  var url = "http://api.openweathermap.org/data/2.5/weather?q=Valladolid&appid=0a1d9df59674b9fc41355d91fc82e893";
+
+  $.ajax(url, {success: function(data){
+    console.log(data);
+    $('.city').text(data.name);
+    $('.temp').text(data.main.temp);
+  }});
+ 
 }
