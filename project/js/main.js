5d465eb00402d3152915f6a8e8fe16aead0cb11e
Eduardo Amigo Nieto <eduardo.amigo@servilia.local>
2020-09-03 13:06:24 +0200

    añadir el tiempo 2

--- project/js/main.js
+++ project/js/main.js
@@ -2,13 +2,23 @@ $(document).ready(function(){
   getWeather();
 })
 
-function getWeather(){
-  var url = "http://api.openweathermap.org/data/2.5/weather?q=Valladolid&appid=0a1d9df59674b9fc41355d91fc82e893";
+function getWeather(varr){
+  var url = "http://api.openweathermap.org/data/2.5/weather?q="+ varr +"&units=metric&appid=0a1d9df59674b9fc41355d91fc82e893";
+
+  $('.city').text("");
+  $('.error-message').text("");
 
   $.ajax(url, {success: function(data){
     console.log(data);
     $('.city').text(data.name);
     $('.temp').text(data.main.temp);
+  }, error: function(error){
+      $(".error-message").text('ha ocurrido un error')
   }});
  
+}
+
+function searchWeather(){
+  var searchQuery = $(".search").val();
+  getWeather(searchQuery);
 }
